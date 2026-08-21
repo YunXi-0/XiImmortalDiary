f94b8fd7def1cb7dcd56fe60bb3a2b7852a8e015
User <user@example.com>
2026-08-21 18:11:01 +0800

    v0.1: 稀有怪物系统 + Android APK构建 + 完整游戏功能

---
+++ outputs/app/src/main/java/com/xiimmortal/game/MainActivity.java
@@ -0,0 +1,34 @@
+package com.xiimmortal.game;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebSettings;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        webView = new WebView(this);
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setDatabaseEnabled(true);
+        settings.setAllowFileAccess(true);
+        webView.setWebViewClient(new WebViewClient());
+        setContentView(webView);
+        webView.loadUrl("file:///android_asset/www/index.html");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
